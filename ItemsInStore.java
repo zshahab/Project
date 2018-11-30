89e8f7bf28eacc39b17af807c6fbde7b7a2db554
AGaudio <36246896+Angela-Gaudio@users.noreply.github.com>
2018-11-30 16:45:42 -0500

    Create ItemsInStore.java

---
+++ ItemsInStore.java
@@ -0,0 +1,13 @@
+/*
+*
+*This class will be using the ListArrayBased ADT to hold several items in the store of the type Item.
+*Author: Angela Gaudio
+*Version: 11/30/2018
+*/
+
+public class ItemsInStore
+{
+    public ItemsInStore()
+    {
+    }
+}
