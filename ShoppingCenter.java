8e32ca56f50a05d2a842f98c21a3f57407db3613
AGaudio <36246896+Angela-Gaudio@users.noreply.github.com>
2018-11-30 16:23:02 -0500

    Create ShoppingCenter.java

---
+++ ShoppingCenter.java
@@ -0,0 +1,14 @@
+/*
+*
+*This class contains a Queue ADT to hold the Customers and had a ListArrayBased ADT to hold the Items
+*Authors: Angela Gaudio & Zain Shahab
+*Version: 11/30/2018
+*/
+
+public class ShoppingCenter
+{
+    
+    public ShoppingCenter()
+    {
+    }
+}
