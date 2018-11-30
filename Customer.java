82be4e39d98a7c47bcc57ad441594d4f311f3a7f
AGaudio <36246896+Angela-Gaudio@users.noreply.github.com>
2018-11-30 16:02:10 -0500

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,5 @@
+/*
+*This class will contain a reference to the Customer within the Shopping Center it will have: String uniqueName and int amountInCart
+*Authors: Angela Gaudio
+*Version: 11/30/2018
+*/
