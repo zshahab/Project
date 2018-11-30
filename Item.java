fe8d6b62fb7a91c72c79c8697c4f1ba412e33149
AGaudio <36246896+Angela-Gaudio@users.noreply.github.com>
2018-11-30 15:59:39 -0500

    Update and rename Item Class to Item.java

---
+++ Item.java
@@ -0,0 +1,43 @@
+/*
+*This class will hold references to items that are in the store and will contain two parameters: int stock and String name
+*Authors: Angela Gaudio
+*Version: 11/30/2018
+*/
+
+public class Item
+{
+      private String itemName;
+      private int stock;
+      public Item()
+      {
+          itemName = null;
+          stock = 0;
+      }
+      
+      public Item(String name, int num)
+      {
+          itemName = name;
+          stock = num;
+      }
+      
+      
+      public String getName()
+      {
+          return itemName;
+      }
+      
+      public int getStock()
+      {
+          return stock;
+      }
+      
+      public void setStock(int newAmount)
+      {
+          stock = stock + newAmount;
+      }
+      
+      public String toString()
+      {
+          return itemName + " with + stock + " items.";
+      }
+}
