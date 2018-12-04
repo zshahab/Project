0682b5f43ed9323e48966e14a54afc38c8f4fc61
AGaudio <36246896+Angela-Gaudio@users.noreply.github.com>
2018-12-03 23:14:35 -0500

    Update ItemsInStore.java

--- ItemsInStore.java
+++ ItemsInStore.java
@@ -8,13 +8,56 @@
 public class ItemsInStore
 {
     ListArrayBased<Item> items;
-    public ItemsInStore()
+    private int restockAmount;
+    public ItemsInStore(int restock)
     {
+        restockAmount = restock;
         items = new ListArrayBased<>();
     }
     
-    public void restockItem(String itemName, int amount)
+    public void addItem(String itemName, int amount)
     {
-        
+        items.add(items.size(), new Item(itemName, amount));
+    }
+    
+    public void needsRestocking()
+    {
+        System.out.println("Items at restocking level: )
+        int size = items.size();
+        for(int i = 0; i < size; i++)
+        {
+          if(items.get(i).getAmount.getStock() <= restockAmount)
+                System.ou.println(items);
+        }   
+    }
+                           
+    public void takeOneItem(String item)
+    {
+       int size = items.size();
+       boolean contin = true;
+       for(int i = 0; i < size && contin; i++)
+       {
+          if((items.get(i).getName()).equals(item))
+          {
+              contin = false;
+              items.get(i).removeOneItem();
+          }
+         
+       }
+    }
+     
+    public void restockItem(String item, int newAmount)
+    {
+       int size = items.size();
+       boolean contin = true;
+      for(int i = 0; i < size && contin; i++)
+      {
+         if((items.get(i).getName()).equals(item))
+         {
+           contin = false;
+           items.get(i).setStock(newAmount);
+           System.out.println("Stock now has " + items.get(i).getStock() + item);
+         }
+      }
     }
 }
