c0a40b5623d7c539d8bfcdbc42329ba7d6a50367
AGaudio <36246896+Angela-Gaudio@users.noreply.github.com>
2018-12-03 23:15:01 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -36,6 +36,11 @@ public class Item
           stock = stock + newAmount;
       }
       
+      public void removeOneItem()
+      {
+            stock--;
+      }
+      
       public String toString()
       {
           return itemName + " with + stock + " items.";
